9d7385f1b0ebc6b3c62cbb64900d8a8487975e37
Samuel Young <samuel.young@softwire.com>
2023-10-23 17:39:32 +0100

    add highlight on the sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,17 +1,27 @@
 import styles from "./Sidebar.module.css";
-import {Link, useLoaderData} from "react-router-dom";
+import {Link, useHref, useLoaderData, useMatch} from "react-router-dom";
 import {RouteObject} from "../../FakeAPI";
+import {useCallback} from "react";
 
 const Sidebar = (props: {
     title: string,
     right?: boolean,
 }) => {
-    const routes = useLoaderData() as RouteObject[]
+    const routes = useLoaderData() as RouteObject[];
+    const href = useHref(".");
+    function useRoute(route: RouteObject) {
+        return <Link
+            className={useMatch(`${href}/${route.id}`) ? styles.ActiveLink : ""}
+            to={route.id}
+        >
+            {route.display}
+        </Link>
+    }
 
     return <div className={props.right ? styles.Sidebar2 : styles.Sidebar}>
         <h2>{props.title}</h2>
         <div className={styles.Links}>
-            {routes.map(route => <Link to={route.id}>{route.display}</Link>)}
+            {routes.map(useRoute)}
         </div>
     </div>
 }
