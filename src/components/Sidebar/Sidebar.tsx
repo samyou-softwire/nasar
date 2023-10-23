e4566f2956800e01a1ce7d9c43ad2ad1498eea67
Samuel Young <samuel.young@softwire.com>
2023-10-23 17:19:07 +0100

    remove sidebar redundancy

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -3,11 +3,12 @@ import {Link, useLoaderData} from "react-router-dom";
 import {RouteObject} from "../../FakeAPI";
 
 const Sidebar = (props: {
-    title: string
+    title: string,
+    right?: boolean,
 }) => {
     const routes = useLoaderData() as RouteObject[]
 
-    return <div className={styles.Sidebar}>
+    return <div className={props.right ? styles.Sidebar2 : styles.Sidebar}>
         <h2>{props.title}</h2>
         <div className={styles.Links}>
             {routes.map(route => <Link to={route.id}>{route.display}</Link>)}
--- src/components/Sidebar/Sidebar2.tsx
+++ src/components/Sidebar/Sidebar2.tsx
@@ -1,18 +1,7 @@
-import styles from "./Sidebar.module.css";
-import {Link, useLoaderData} from "react-router-dom";
-import {RouteObject} from "../../FakeAPI";
+import Sidebar from "./Sidebar";
 
 const Sidebar2 = (props: {
-    title: string
-}) => {
-    const routes = useLoaderData() as RouteObject[]
-
-    return <div className={styles.Sidebar2}>
-        <h2>{props.title}</h2>
-        <div className={styles.Links}>
-            {routes.map(route => <Link to={route.id}>{route.display}</Link>)}
-        </div>
-    </div>
-}
+    title: string,
+}) => <Sidebar title={props.title} right/>
 
 export default Sidebar2;
