d53466de74fc80bff3bbc73f888f5d7ccbce0252
Samuel Young <samuel.young@softwire.com>
2023-10-24 09:27:09 +0100

    make the homepage display again

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -1,9 +1,11 @@
-import {Link, Outlet, useLoaderData} from "react-router-dom";
+import {Link, Outlet, useLoaderData, useMatch} from "react-router-dom";
 import styles from "./Layout.module.css";
 import {RouteObject} from "../FakeAPI";
+import Home from "./home/Home";
 
 const Layout = () => {
     const objects = useLoaderData() as RouteObject[];
+    const onHomepage = useMatch("/");
 
     return <div className={styles.Layout}>
         <header className={styles.Header}>
@@ -12,7 +14,7 @@ const Layout = () => {
                 {objects.map(route => <Link key={route.id} to={route.id}>{route.display}</Link>)}
             </div>
         </header>
-        <Outlet/>
+        {onHomepage ? <Home/> : <Outlet/>}
         <footer className={styles.Footer}>
             <Link to={"/"}>Home</Link>
         </footer>
