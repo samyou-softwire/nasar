d07c659d27d4e68b8d1167fb46e13353a69c6204
Samuel Young <samuel.young@softwire.com>
2023-10-24 12:24:36 +0100

    implement spinner
    it's random

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -1,11 +1,14 @@
-import {Link, Outlet, useMatch} from "react-router-dom";
+import {Link, Outlet, useMatch, useNavigation} from "react-router-dom";
 import styles from "./Layout.module.css";
 import Home from "./home/Home";
+import Spinner from "../components/Spinner/Spinner";
 
 const Layout = () => {
     const onHomepage = useMatch("/");
+    const navigation = useNavigation();
 
     return <div className={styles.Layout}>
+        {navigation.state === "loading" && <Spinner/>}
         <header className={styles.Header}>
             <h1>NASAR</h1>
             <div className={styles.Links}>
